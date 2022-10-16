7f87b1c76b67688c2cb8592b15e77e6af8bfaf86
Antwan <antwansittard@gmail.com>
2022-10-16 19:58:25 +0200

    added git

---
+++ src/main/java/com/Antwan/AccountSystem/model/google.java
@@ -0,0 +1,15 @@
+package com.Antwan.AccountSystem.model;
+
+public class google {
+
+    private String _id;
+
+    public google(String id){
+        _id = id;
+    }
+
+    public String getId(){
+        return _id;
+    }
+
+}
